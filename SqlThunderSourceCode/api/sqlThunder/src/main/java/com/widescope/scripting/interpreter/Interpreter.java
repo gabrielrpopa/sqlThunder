06f5321148e4d133425edfb8aeefa75e66712da8
Gabriel R. Popa <80181538+gabrielrpopa@users.noreply.github.com>
2024-11-07 18:02:51 -0500

    Add files via upload
    Signed-off-by: Gabriel R. Popa <80181538+gabrielrpopa@users.noreply.github.com>

---
+++ SqlThunderSourceCode/api/sqlThunder/src/main/java/com/widescope/scripting/interpreter/Interpreter.java
@@ -0,0 +1,16 @@
+package com.widescope.scripting.interpreter;
+
+public class Interpreter {
+
+    public static String PYTHON2 = "PYTHON2";
+    public static String PYTHON3 = "PYTHON3";
+    public static String GROOVY = "GROOVY";
+    public static String PERL = "PERL";
+    public static String R = "R";
+    public static String RUBY = "RUBY";
+    public static String WINDOWS_BATCH = "WINDOWS BATCH";
+    public static String BASH = "BASH";
+    public static String POWERSHELL = "POWERSHELL";
+    public static String JULIA = "JULIA";
+    
+}
