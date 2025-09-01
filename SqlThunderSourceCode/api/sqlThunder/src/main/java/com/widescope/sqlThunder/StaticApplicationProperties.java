3e0eb4e79fcea7c5e4496012ed8b7d75068f0c3b
Gabriel R. Popa <80181538+gabrielrpopa@users.noreply.github.com>
2025-09-01 15:34:24 -0400

    Delete SqlThunderSourceCode directory
    Signed-off-by: Gabriel R. Popa <80181538+gabrielrpopa@users.noreply.github.com>

--- SqlThunderSourceCode/api/sqlThunder/src/main/java/com/widescope/sqlThunder/StaticApplicationProperties.java
+++
@@ -1,9 +0,0 @@
-package com.widescope.sqlThunder;
-
-public class StaticApplicationProperties {
-    public static String staticIp = "";
-    public static final String localHost = "localhost";
-    public static String localHttpPort = "9099";
-    public static String applicationOwner = "";
-    public static int userSessionCacheLocation = -1; /*The cache server can be local - 0, or another node - 1, holds session */
-}
